c754580ad7e9311b39bc960056841ec36955e187
PAO <ivan.seleznyov51@gmail.com>
2022-11-07 23:51:47 +0300

    test connection MySql(DB browser)

---
+++ src/main/resources/test.sql
@@ -0,0 +1,7 @@
+#insert into users (name, age, email) values('Ivan', 28, 'ivan.seleznyov51@gmail.com');
+#insert into users (name, age, email) values('Бурый', 3, 'msviter@.bu');
+select * from users;
+#select  name ,age from users where idusers = 1;
+#select * from users where name = 'Katya';
+#update users set name = 'Katya', age = 25, email = 'hz@' where idusers = 5;
+#delete from users where idusers = 7;
