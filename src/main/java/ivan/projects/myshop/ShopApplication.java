5a042bb26fd4ddf9ec07ee01815cbdffe543c368
PAO <ivan.seleznyov51@gmail.com>
2022-11-14 00:09:51 +0300

    Connection MySQL(JDBC)

--- src/main/java/ivan/projects/myshop/ShopApplication.java
+++ src/main/java/ivan/projects/myshop/ShopApplication.java
@@ -1,9 +1,34 @@
 package ivan.projects.myshop;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ShopApplication {
 
-    public static void main(String[] args) {
-        System.out.println("type");
-    }
+   private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+   public static final String USERNAME = "qiwikaki";
+   public static final String PASSWORD = "phantom1!56AS";
 
+
+    public static void main(String[] args) {
+
+        Connection connection;
+
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connect to the DB access!");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("Disconnect to the DB!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to load driver class");
+        }
+    }
 }
