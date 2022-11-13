a8f103675d4d3bd4f7efd22245c01ad7d52d1561
PAO <ivan.seleznyov51@gmail.com>
2022-11-14 00:45:34 +0300

    statement

--- src/main/java/ivan/projects/myshop/ShopApplication.java
+++ src/main/java/ivan/projects/myshop/ShopApplication.java
@@ -1,9 +1,6 @@
 package ivan.projects.myshop;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ShopApplication {
 
@@ -14,21 +11,42 @@ public class ShopApplication {
 
     public static void main(String[] args) {
 
-        Connection connection;
-
         try {
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Connect to the DB access!");
-            }
-            connection.close();
-            if (connection.isClosed()) {
-                System.out.println("Disconnect to the DB!");
-            }
         } catch (SQLException e) {
-            System.err.println("Failed to load driver class");
+            e.printStackTrace();
         }
+
+        try {
+            Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            Statement statement = connection.createStatement();
+//            statement.execute("INSERT INTO users (name, age, email) values('Пингвин', 5, 'kinder@');");
+//            int res = statement.executeUpdate("UPDATE users SET name = 'Пингвин' WHERE idusers = 11;");
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+//            statement.addBatch("");
+//            boolean status = statement.isClosed();
+//            statement.getConnection();
+//            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+//        Connection connection;
+//
+//        try {
+//            Driver driver = new com.mysql.cj.jdbc.Driver();
+//            DriverManager.registerDriver(driver);
+//            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+//            if (!connection.isClosed()) {
+//                System.out.println("Connect to the DB access!");
+//            }
+//            connection.close();
+//            if (connection.isClosed()) {
+//                System.out.println("Disconnect to the DB!");
+//            }
+//        } catch (SQLException e) {
+//            System.err.println("Failed to load driver class");
+//        }
     }
 }
