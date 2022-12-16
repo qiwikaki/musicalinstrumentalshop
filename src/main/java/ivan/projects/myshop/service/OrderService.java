13986e6cf99edf5ba5d3e183c995d2feccde4146
Ivan <47557610+qiwikaki@users.noreply.github.com>
2022-12-16 16:38:55 +0300

    "service ch1"

--- src/main/java/ivan/projects/myshop/service/OrderService.java
+++ src/main/java/ivan/projects/myshop/service/OrderService.java
@@ -1,25 +1,33 @@
 package ivan.projects.myshop.service;
 
-import ivan.projects.myshop.dao.ClientDaoImpl;
-import ivan.projects.myshop.dao.InstrumentDaoImpl;
-import ivan.projects.myshop.dao.OrderDaoImpl;
+import ivan.projects.myshop.dao.*;
 import ivan.projects.myshop.entity.Client;
 import ivan.projects.myshop.entity.Instrument;
 import ivan.projects.myshop.entity.Order;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public class OrderService {
 
-    private OrderDaoImpl orderDaoimpl;
-    private ClientDaoImpl clientDaoimpl;
-    private InstrumentDaoImpl instrumentDaoIpl;
+//    private OrderDao orderDao;
+//    private ClientDao clientDao;
+//    private InstrumentDao instrumentDao;
+    private static int AUKey;
 
     public void createOrder() {
+        ClientDaoImpl clientDao = new ClientDaoImpl();
+        InstrumentDaoImpl instrumentDao = new InstrumentDaoImpl();
+        OrderDaoImpl orderDao = new OrderDaoImpl();
+
+        clientDao.createEntity(new Client());
+        instrumentDao.createEntity(new Instrument());
+        orderDao.createEntity(new Order(AUKey++, LocalDateTime.now(), ));
 
     }
 
     public static void main(String[] args) {
+
         OrderService orderService = new OrderService();
         orderService.createOrder();
     }
